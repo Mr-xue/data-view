d2640d3597a0c6c89da227fd16cdd80c3430f967
菠菜 <360240422@qq.com>
2020-08-19 15:04:56 +0800

    first

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 将自动注册所有组件为全局组件
+import dataV from '@jiaminghi/data-view'
+Vue.use(dataV)
+
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <dv-full-screen-container>
+          <!--加载loading -->
+          <dv-loading v-if="loading">Loading...</dv-loading>
+
+          <div class="d-cont">
+              <dv-border-box-1>dv-border-box-1</dv-border-box-1>
+          </div>
+      </dv-full-screen-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data() {
+      return {
+          loading : true,
+      }
+  },
+  mounted() {
+      setTimeout(()=>{
+          this.loading = false;
+      },1500)
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="less">
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.home{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: url(../assets/bg.png);
+    background-size: cover;
+    background-position: 50% 50%;
+}
+#dv-full-screen-container{
+    .d-cont{
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+    }
+    // 修改loading样式
+    .dv-loading .loading-tip{
+        color:#fff;
+        font-weight: bold;
+        font-size: 16px;
+    }
+}
+</style>
